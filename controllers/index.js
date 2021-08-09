5b2a4e0e1992b62a3bb3d8357ee92bcb7515ca55
Davelpz-tech <davelpz15@gmail.com>
2021-08-08 21:53:35 -0700

    fixing routes connections, fixing edit button position in html (handlebars)

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require("./home-routes")
-const dashboardRoutes = require("./dashboard-routes")
+const homeRoutes = require("./home-routes");
+const dashboardRoutes = require("./dashboard-routes");
+// const postRoutes = require('./post-routes');
 
 router.use("/", homeRoutes)
 router.use('/api', apiRoutes);
-router.use("/dashboard", dashboardRoutes)
+router.use("/dashboard", dashboardRoutes);
+// router.use("/posts", postRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
